090ab87d70478365cb70bc979d7b5157410b28ec
pozdniakova olga <caramelca07@mail.ru>
2023-09-20 15:55:36 +0200

    Added field "lessons" for CourseSerializer

--- main/serializers.py
+++ main/serializers.py
@@ -3,21 +3,22 @@ from rest_framework import serializers
 from main.models import Course, Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
-    """Сериализатор для модели Курс"""
-    lesson_count = serializers.SerializerMethodField(read_only=True)
-
-    class Meta:
-        model = Course
-        fields = ['id', 'title', 'description', 'lesson_count']
-
-    def get_lesson_count(self, instance):
-        return instance.lesson_set.all().count()
-
-
 class LessonSerializer(serializers.ModelSerializer):
     """Сериализатор для модели Урок"""
 
     class Meta:
         model = Lesson
         fields = '__all__'
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Курс"""
+    lesson_count = serializers.SerializerMethodField(read_only=True)
+    lessons = LessonSerializer(source='lesson_set', many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = ['id', 'title', 'description', 'lesson_count', 'lessons']
+
+    def get_lesson_count(self, instance):
+        return instance.lesson_set.all().count()
